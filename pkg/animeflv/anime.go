e59b6f5daad3e4e11b9cc77b4791db8af28e3639
Jefri <jefrijmartinez@gmail.com>
2020-06-24 20:10:26 -0400

    Update to animeflv package for anime decoding xml

--- pkg/animeflv/anime.go
+++ pkg/animeflv/anime.go
@@ -8,7 +8,14 @@ import (
 	"time"
 
 	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html"
 )
+
+// AnimeUtils contains encode definition method for
+// fill up anime information once obtained.
+type AnimeUtils interface {
+	DecodeNode(node *html.Node) error
+}
 
 // Anime holds attribute values.
 type Anime struct {
@@ -22,65 +29,80 @@ type Anime struct {
 	Genres           []string   `json:"genres"`
 }
 
+// DecodeNode  returbs
+func (a *Anime) DecodeNode(node *html.Node) error {
+	title, err := getTextFromNode(node, titleXpath)
+	if err != nil {
+		return err
+	}
+	a.Title = title
+
+	typeValue, err := getTextFromNode(node, typeXpath)
+	if err != nil {
+		return err
+	}
+	a.Type = typeValue
+
+	cover, err := getTextFromNode(node, coverXpath)
+	if err != nil {
+		return err
+	}
+	a.Cover = cover
+
+	status, err := getTextFromNode(node, statusXpath)
+	if err != nil {
+		return err
+	}
+	a.Status = status
+
+	geners, err := htmlquery.QueryAll(node, genresXpath)
+	if err != nil {
+		return err
+	}
+
+	for _, itemNode := range geners {
+		if itemNode == nil {
+			return nil
+		}
+		a.Genres = append(a.Genres, htmlquery.InnerText(itemNode))
+	}
+
+	synopsis, err := getTextFromNode(node, synopsisXpath)
+	if err != nil {
+		return err
+	}
+	a.Synopsis = synopsis
+
+	scriptContent, err := getTextFromNode(node, scriptContentXpath)
+	if err != nil {
+		return err
+	}
+
+	nextRelease, err := getNextReleaseDateFromString(scriptContent)
+	if err != nil {
+		return err
+	}
+	a.NextRelease = nextRelease
+
+	lastEpisode, err := getLastEpisodeFromString(scriptContent)
+	if err != nil {
+		return err
+	}
+	a.LastAiredEpisode = lastEpisode
+	return nil
+}
+
 // GetAnime returns pointer to a new single anime representation.
 func GetAnime(sourceURL string) (*Anime, error) {
 	doc, err := htmlquery.LoadURL(sourceURL)
 	if err != nil {
 		return nil, err
 	}
-
-	titleNode, err := htmlquery.Query(doc, titleXpath)
-	if err != nil {
-		return nil, err
-	}
-
-	typeNode, err := htmlquery.Query(doc, typeXpath)
-	if err != nil {
-		return nil, err
-	}
-
-	coverNode, err := htmlquery.Query(doc, coverXpath)
-	if err != nil {
-		return nil, err
-	}
-
-	statusNode, err := htmlquery.Query(doc, statusXpath)
-	if err != nil {
-		return nil, err
-	}
-
-	genresNodes, err := htmlquery.QueryAll(doc, genresXpath)
-	if err != nil {
-		return nil, err
-	}
-
-	synopsisNode, err := htmlquery.Query(doc, synopsisXpath)
-	if err != nil {
-		return nil, err
-	}
-
-	contentScriptNode, err := htmlquery.Query(doc, scriptContentXpath)
-	if err != nil {
-		return nil, err
-	}
-
 	anime := &Anime{}
-	anime.Title = htmlquery.InnerText(titleNode)
-	anime.Type = htmlquery.InnerText(typeNode)
-	anime.Cover = fmt.Sprintf("%s%s", hostname, htmlquery.InnerText(coverNode))
-	anime.Status = htmlquery.InnerText(statusNode)
-	for _, genre := range genresNodes {
-		anime.Genres = append(anime.Genres, htmlquery.InnerText(genre))
+	err = anime.DecodeNode(doc)
+	if err != nil {
+		return nil, err
 	}
-
-	contentScript := htmlquery.InnerText(contentScriptNode)
-	nextRelease, _ := getNextReleaseDateFromString(contentScript)
-	anime.NextRelease = nextRelease
-
-	lastEpisode, _ := getLastEpisodeFromString(contentScript)
-	anime.LastAiredEpisode = lastEpisode
-
-	anime.Synopsis = htmlquery.InnerText(synopsisNode)
 	return anime, nil
 }
 
@@ -90,6 +112,10 @@ func GetAllCurrentlyAiringShows() ([]Anime, error) {
 	sources, err := GetSources()
 	if err != nil {
 		return nil, err
+	}
+
+	if len(sources) == 0 {
+		return nil, errors.New("There are no sources available to load new anime data")
 	}
 
 	var animes []Anime
@@ -104,6 +130,20 @@ func GetAllCurrentlyAiringShows() ([]Anime, error) {
 	return animes, nil
 }
 
+// getTextFromNode returns inner text from a specific node.
+func getTextFromNode(doc *html.Node, xpath string) (innerText string, err error) {
+	node, err := htmlquery.Query(doc, xpath)
+	if err != nil {
+		return "", err
+	}
+	if node == nil {
+		return "", fmt.Errorf("There is an error trying to get node with specified xpath: %v", xpath)
+	}
+	nodeValue := htmlquery.InnerText(node)
+	return nodeValue, nil
+}
+
+// getNextReleaseDateFromString returns time from current scriptContent.
 func getNextReleaseDateFromString(contentScript string) (*time.Time, error) {
 	re := regexp.MustCompile(`[0-9]{4}-[0-9]{2}-[0-9]{2}`)
 	results := re.FindAllString(contentScript, 1)
@@ -119,6 +159,7 @@ func getNextReleaseDateFromString(contentScript string) (*time.Time, error) {
 	return nil, errors.New("It couldn't find value to parse within content script")
 }
 
+// getLastEpisodeFromString returns the last episode on air.
 func getLastEpisodeFromString(contentScript string) (int, error) {
 
 	re := regexp.MustCompile(`\[([0-9]+)\,`)
